25d092ad4aad1a9c6289a28431d41b31c18519a4
niyizibyose <jean21101999@gmail.com>
2022-07-30 13:11:27 +0200

    newphoto

--- src/modules/_partials/HomeSlides/index.tsx
+++ src/modules/_partials/HomeSlides/index.tsx
@@ -22,12 +22,12 @@ const resources = [
     link: '/about-2',
     about: 'Law firm in Rwanda',
   },
- // {
-   // title: 'Pro Bono',
-   // imageUrl: '/assets/images/slide2.jpg',
-   // link: '/about-2',
-   // about: 'Justice for All',
-//},
+ {
+   title: 'The Certa Foundation',
+   imageUrl: '/assets/images/foundation.jpg',
+   ink: '/about-2',
+   about: '',
+},
 ];
 
 const HomeSlides = ({ data = resources }: { data?: IItem[] }) => {
